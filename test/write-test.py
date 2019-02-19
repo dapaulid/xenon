bd61cb59dbc2a7e75f38f383b93f3147973d1b08
Daniel Pauli <dapaulid@gmail.com>
2019-02-19 18:18:16 +0100

    Output errors and warnings.

--- test/write-test.py
+++ test/write-test.py
@@ -8,7 +8,15 @@ entry = 0
 with open("test.log", "w") as text_file:
 	while True:
 		entry += 1
-		text_file.write("This is entry %d!\n" % entry)
-		text_file.write("  some more info")
+		if entry % 8 != 0:
+			text_file.write("This is entry %d!\n" % entry)
+			text_file.write("  some more info\n")
+		else:
+			text_file.write("Some warning happened here\n")
+			text_file.write("Some error happened here\n")
+		# end if
 		text_file.flush()
 		time.sleep(1)
+	# end while
+# end with
+
