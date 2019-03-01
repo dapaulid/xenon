79b8eaa8de2a011d643d7d4ad1a8a1f569377083
Daniel Pauli <dapaulid@gmail.com>
2019-03-01 14:49:14 +0100

    Display message box for warning and error traces.

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,27 @@
 #include <QFile>
 #include <QTextStream>
 #include <QDebug>
+#include <QMessageBox>
 
 #include "logfile.h"
+
+void displayLogMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    Q_UNUSED(context);
+    switch (type) {
+    case QtWarningMsg:
+        QMessageBox::warning(nullptr, "Warning", msg);
+        break;
+    case QtCriticalMsg:
+        QMessageBox::critical(nullptr, "Critical Error", msg);
+        break;
+    case QtFatalMsg:
+        QMessageBox::critical(nullptr, "Fatal Error", msg);
+        break;
+    default:
+        ;// do nothing
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -13,9 +32,11 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    qInstallMessageHandler(displayLogMessageHandler);
     qSetMessagePattern("%{time yyyy-MM-ddTHH:mm:ss.zzz} %{if-debug}D%{endif}%{if-info}I%{endif}%{if-warning}W%{endif}%{if-critical}C%{endif}%{if-fatal}F%{endif} %{file}:%{line} - %{message}");
 
     QApplication a(argc, argv);
+
     CMainWindow w;
     w.show();
 
